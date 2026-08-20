2da26f90d1488de0aa4aba5e651feb1ed2901d31
ISADORA FERNANDA DA SILVA RIFFEL <isadora_riffel@estudante.sesisenai.org.br>
2026-08-19 21:47:35 -0300

    tem rever isso aqui

--- js/script.js
+++ js/script.js
@@ -9,3 +9,28 @@ document.getElementById('formLogin').addEventListener('submit', function(event) 
         window.location.href = 'espurr.html'
     }, 1000)
 })
+
+const usuariosValidos = {
+    "fizzy": "admin123",
+    "Ash": "pikachu123",
+    "N": "soltapokemon"
+};
+
+
+var loginForm = document.querySelector('#id01 form');
+
+loginForm.addEventListener('submit', function(event){
+    event.preventDefault();
+
+    var usuario = loginForm.uname.value.trim();
+    var senha = loginForm.psw.value;
+
+    if (usuariosValidos.hasOwnProperty(usuario)&&usuariosValidos[usuario]===senha){
+        alert("Sucesso!!!");
+        modal.style.display = "none";
+        loginForm.reset();
+    }else{alert("usuario incorreto :(");
+
+    }
+     
+});
